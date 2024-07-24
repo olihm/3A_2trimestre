15945c24fef7087d7ee5778852744c20c589a059
olihm <106966316+olihm@users.noreply.github.com>
2024-07-24 08:37:14 -0300

    .

--- script.js
+++ script.js
@@ -42,3 +42,21 @@ const perguntas = [
         ]
     }
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
