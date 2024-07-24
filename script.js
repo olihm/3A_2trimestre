28a2e866f8ce52cefd853a7435ceee54460f76bf
olihm <106966316+olihm@users.noreply.github.com>
2024-07-24 08:34:16 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-}
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
